6c7035801b9387b8d25a283e190d11d4585b070c
Fahim Rayhan <fahimrayhan786@gmail.com>
2023-08-11 23:22:13 +0600

    show password option added

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -4,6 +4,19 @@ import { useSignup } from "../hooks/useSignup";
 import { Helmet } from "react-helmet";
 
 const SignUp = () => {
+
+  // Show Password
+  const [passwordType, setPasswordType] = useState("password");
+
+  const togglePassword = (e) => {
+    e.preventDefault()
+    if (passwordType === "password") {
+      setPasswordType("text")
+      return;
+    }
+    setPasswordType("password")
+  }
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [conf_password, setConfPassword] = useState("");
@@ -58,7 +71,7 @@ const SignUp = () => {
             </p>
 
             <form onSubmit={handleSubmit}>
-              <div className="mt-5">
+              <div className="mt-5 ">
                 <input
                   onChange={(e) => setEmail(e.target.value)}
                   value={email}
@@ -67,14 +80,30 @@ const SignUp = () => {
                   className="border border-gray-400 px-2 py-1 w-full focus:outline-orange-500 focus:shadow-lg"
                 />
               </div>
-              <div className="mt-5">
+              <div className="mt-5 relative">
                 <input
+
+                  type={passwordType} 
                   onChange={(e) => setPassword(e.target.value)}
                   value={password}
-                  type="password"
                   placeholder="Password"
-                  className="border border-gray-400 px-2 py-1 w-full focus:outline-orange-500 focus:shadow-lg"
+                  className="border border-gray-400 px-2 py-1 w-full focus:outline-orange-500 focus:shadow-lg "
                 />
+
+                <button className="absolute inset-y-0 right-0 flex items-center px-4 text-gray-600" onClick={(e)=>{
+                    togglePassword(e)
+                  }}>
+                    {passwordType === "password" ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-600">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+                    </svg>
+
+
+                      : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-600">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                      </svg>}
+                  </button>
+                
               </div>
               <div className="mt-5">
                 <input

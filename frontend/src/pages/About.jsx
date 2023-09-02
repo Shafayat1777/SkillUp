bdd6ff83dd86cb0bf063bb1e4ce039ad551bb4f9
Shafayat1777 <shafayat1777@gmail.com>
2023-09-02 09:01:37 +0600

    fixed some css

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -53,60 +53,64 @@ const About = () => {
       </div>
 
       <div>
-        <div className="border m-10 h-[48rem] flex flex-col">
+        <div className="border m-10 h-[48rem] flex flex-col justify-between">
           <div className="p-5 border-b text-2xl font-semibold text-gray-600">
             ChatRoom
           </div>
-          <div className="flex-grow flex flex-col ">
-            <div className="p-5 flex-grow flex text-gray-600 font-normal text-lg items-end overflow-auto">
-              <div>
-                {output.length > 0 && (
-                  <div>
-                    {output.map((data, i) => (
-                      <p
-                        key={i}
-                        className={`mb-2 border px-3 py-1 rounded-full bg-slate-100 ${user.id===data.id? "bg-blue-500 text-white":""}`}
-                      >
-                        {data.message}
-                      </p>
-                    ))}
-                  </div>
-                )}
-                {feedback && (
-                  <div className="px-3 py-1 text-gray-400">
-                    <em>{feedback + " is typing..."}</em>
-                  </div>
-                )}
-              </div>
-            </div>
-            <div className="border-t p-5 flex items-center">
-              <input
-                onKeyDown={(e) => handleFeedback(e)}
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                type="text"
-                placeholder="Type something..."
-                className="border rounded-full p-2 w-full"
-              />
-              <div
-                onClick={handleSendMessage}
-                className=" ml-5 border rounded-full p-2 flex items-center justify-center hover:bg-blue-400 cursor-pointer"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="white"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1"
-                  stroke="currentColor"
-                  className="w-6 h-6 text-blue-400"
+
+          <div className=" p-5 text-gray-600 h-full font-normal text-lg items-end overflow-auto flex flex-col justify-end">
+            {output.length > 0 &&
+              output.map((data, i) => (
+                <div
+                  className={` flex p-3 ${
+                    user.id === data.id ? "justify-end" : "mr-auto"
+                  }`}
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-                  />
-                </svg>
+                  <p
+                    key={i}
+                    className={` border px-3 py-1 rounded-full bg-slate-100 ${
+                      user.id === data.id ? "bg-blue-500 text-white" : ""
+                    }`}
+                  >
+                    {data.message}
+                  </p>
+                </div>
+              ))}
+
+            {feedback && (
+              <div className="px-3 py-2 text-gray-400">
+                <em>{feedback + " is typing..."}</em>
               </div>
+            )}
+          </div>
+
+          <div className="border-t p-5 flex items-center">
+            <input
+              onKeyDown={(e) => handleFeedback(e)}
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              type="text"
+              placeholder="Type something..."
+              className="border rounded-full p-2 w-full"
+            />
+            <div
+              onClick={handleSendMessage}
+              className=" ml-5 border rounded-full p-2 flex items-center justify-center hover:bg-blue-400 cursor-pointer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="white"
+                viewBox="0 0 24 24"
+                strokeWidth="1"
+                stroke="currentColor"
+                className="w-6 h-6 text-blue-400"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+                />
+              </svg>
             </div>
           </div>
         </div>
